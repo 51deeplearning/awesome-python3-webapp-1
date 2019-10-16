26ca3db1279f4c4ab97aa85150905581302e8afa
51deeplearning <register_for_me@yeah.net>
2019-10-16 21:53:39 +0800

    截止到day-07分支

---
+++ www/handlers.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+'url handlers'
+
+import re
+import time
+import json
+import logging
+import hashlib
+import base64
+import asyncio
+from .coroweb import get, post
+from .models import User, Comment, Blog, next_id
+
+
+@get('/')
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', nmae='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
+    return{
+        '__template__': 'blogs.html',
+        'blogs': blogs
+    }
